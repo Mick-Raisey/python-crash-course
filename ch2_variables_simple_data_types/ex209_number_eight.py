0eb85a951933fbbd0b0af1808caca36afb734604
Mick-Raisey <tabs4bass@gmail.com>
2024-09-29 12:34:03 +0930

    completed chapter 2

---
+++ ch2_variables_simple_data_types/ex209_number_eight.py
@@ -0,0 +1,6 @@
+# 2-9. Number Eight
+# All result in he number 8
+print(3 + 5)
+print(16 / 2)
+print(4 * 2)
+print(10 - 2)
---
+++ ch2_variables_simple_data_types/ex210_favorite_number.py
@@ -0,0 +1,7 @@
+# 2-10. Favorite Number 
+# Use a variable to represent your favorite number. 
+# Then, using that variable, create a message that reveals your favorite number. 
+# Print that message.
+fav_number = 13
+message =f"My favourite number is {fav_number}"
+print(message)
