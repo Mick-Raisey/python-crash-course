bc45a0bdb408975ac3df1e1644f9cd0fe90f4fe1
Mick-Raisey <tabs4bass@gmail.com>
2024-09-29 13:21:02 +0930

    starting chapter 3

---
+++ ch3_introducing_lists/ex302_greetings.py
@@ -0,0 +1,10 @@
+# 3-2. Greetings
+# Print a message each person
+# The text of each message should be the same,
+# but each message should be personalized with the person’s name.
+names = ['freddie mercury', 'bon scott', 'ace frehley', 'chuck berry']
+message ="Welcome to my website"
+print(f"{message} {names[0].title()}!")
+print(f"{message} {names[1].title()}!")
+print(f"{message} {names[2].title()}!")
+print(f"{message} {names[3].title()}!")
