ad635221853e6f8866dcf7f1e781a7145659105e
Mick-Raisey <tabs4bass@gmail.com>
2024-09-29 15:46:56 +0930

    added 304

---
+++ ch3_introducing_lists/ex304_guest_list.py
@@ -0,0 +1,16 @@
+# 3-4. Guest List
+# Make a list that includes at least three people you’d like to invite to dinner.
+# Then use your list to print a message to each person, inviting them to dinner.
+guests = ['malcolm young', 'ace frehley', 'john lennon', 'freddie mercury']
+
+guest1 = guests[0].title()
+print(f"{guest1}, You are invited to the party")
+
+guest2 = guests[1].title()
+print(f"{guest2}, You are invited to the party")
+
+guest3 = guests[2].title()
+print(f"{guest3}, You are invited to the party")
+
+guest4 = guests[3].title()
+print(f"{guest4}, You are invited to the party")
