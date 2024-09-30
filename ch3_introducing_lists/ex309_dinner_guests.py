5b7c9e835abad2a006ce4f699afca44cb529749b
Mick-Raisey <tabs4bass@gmail.com>
2024-09-30 14:24:44 +0930

    added 309 len function

---
+++ ch3_introducing_lists/ex309_dinner_guests.py
@@ -0,0 +1,5 @@
+# 3-4. Guest List
+# Print the length of the guests list
+guests = ['malcolm young', 'ace frehley', 'john lennon', 'freddie mercury']
+length = len(guests)
+print(f"There are {length} people invited to the party")
