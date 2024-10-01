f6d66ecfbe689e92666fb8b2f4296e96d713e3c3
Mick-Raisey <tabs4bass@gmail.com>
2024-10-01 12:08:07 +0930

    added 402

---
+++ ch4_working_with_lists/ex402_animals.py
@@ -0,0 +1,15 @@
+# 4-2. Animals
+
+# Modify your program to print a statement about each animal, such as 
+#   A dog would make a great pet.
+# Add a line at the end of your program, stating what these animals have in common.
+#   You could print a sentence, such as 
+#   Any of these animals would make a great pet!
+
+animals = ['dog', 'cat', 'horse', 'bird']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet')
+
+print('All these animals have four legs')
+print('Any of these animals would make a great pet!')
