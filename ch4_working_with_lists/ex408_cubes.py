56a5e2d7a2f55bbf41786485a3e718ba8f4d2b0f
Mick-Raisey <tabs4bass@gmail.com>
2024-10-02 17:35:42 +0930

    added 408

---
+++ ch4_working_with_lists/ex408_cubes.py
@@ -0,0 +1,9 @@
+# 4-8. Cubes
+
+# Make a list of the first 10 cubes
+# and use a for loop to print out the value of each cube.
+
+cubes = [i**3 for i in range(1, 11)]
+
+for cube in cubes:
+    print(cube)
