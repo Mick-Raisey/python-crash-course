5952da5765e95bb8f361f33a56c7f339bbd0f944
Mick-Raisey <tabs4bass@gmail.com>
2024-10-04 12:57:34 +0930

    added 507

---
+++ ch5_if_statements/ex507_favourite_fruit.py
@@ -0,0 +1,12 @@
+# 5-7. Favorite Fruit: 
+
+fav_fruits = ['banana', 'apricot', 'pear', 'orange']
+
+if 'banana' in fav_fruits:
+    print('You really like bananas!')
+if 'apple' in fav_fruits:
+    print('You really like apples!')
+if 'pear' in fav_fruits:
+    print('You really like pears!')
+if 'orange' in fav_fruits:
+    print('You really like oranges')
