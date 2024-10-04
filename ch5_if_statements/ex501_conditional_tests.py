80c77a6aef79b2ebd5c367f61ea2da282a14ec82
Mick-Raisey <tabs4bass@gmail.com>
2024-10-04 12:20:03 +0930

    added 501

---
+++ ch5_if_statements/ex501_conditional_tests.py
@@ -0,0 +1,14 @@
+# 5-1. Conditional Tests
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True")
+print(car == 'subaru')
+
+print("\nIs car == 'SuBaRu'? I predict True")
+print(car.lower() == 'subaru')
+
+print("\nIs car == 'ferrari'? I predict False")
+print(car == 'ferrari')
+
+print("\nIs car != 'audi'? I predict True")
+print(car != 'audi')
