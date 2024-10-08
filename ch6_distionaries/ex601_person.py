85f64c5a4025db4dac93912f66828f7b1757ca43
Mick-Raisey <tabs4bass@gmail.com>
2024-10-08 12:18:44 +1030

    added 601

---
+++ ch6_distionaries/ex601_person.py
@@ -0,0 +1,12 @@
+# 6-1. Person
+
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 21,
+    'city': 'Rockville',
+}
+
+print(f"Name: {person['first_name']} {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
