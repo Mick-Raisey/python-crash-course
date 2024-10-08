a6bfb0abcd426251d2663b0280eec51f60f0ed4a
Mick-Raisey <tabs4bass@gmail.com>
2024-10-08 12:49:38 +1030

    added 603

---
+++ ch6_distionaries/ex603_glossary.py
@@ -0,0 +1,13 @@
+# 6-3. Glossary
+
+glossary = {
+    'list': 'a collection of item in order',
+    'comment': 'a note that is ignored by the interpreter',
+    'loop': 'go through a collection of items one at a time',
+    'dictionary': 'a collection of items with a key value pair',
+}
+
+print(f"String:\n\t{glossary['list']}")
+print(f"Comment:\n\t{glossary['comment']}")
+print(f"Loop:\n\t{glossary['loop']}")
+print(f"Dictionary:\n\t{glossary['dictionary']}")
