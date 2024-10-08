c88efe9d36b86fe6b9b8b4e4bf46cdf541fedf2f
Mick-Raisey <tabs4bass@gmail.com>
2024-10-08 12:25:25 +1030

    added 602

---
+++ ch6_distionaries/ex602_favorite_numbers.py
@@ -0,0 +1,19 @@
+# 6-2. Favorite Numbers
+
+# Think of a favorite number for each person, 
+# and store each as a value in your dictionary. 
+# Print each person's name and their favorite number.
+
+fav_numbers = {
+    'john': 21,
+    'mick': 13,
+    'jack': 6,
+    'mimi': 30,
+    'sid': 10,
+}
+
+print(f"John's favourite number is {fav_numbers['john']}")
+print(f"Mick's favourite number is {fav_numbers['mick']}")
+print(f"Jack's favourite number is {fav_numbers['jack']}")
+print(f"Mimi's favourite number is {fav_numbers['mimi']}")
+print(f"Sid's favourite number is {fav_numbers['sid']}")
